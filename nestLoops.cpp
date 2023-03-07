e8306e059beb007a189a59f504c273e0b8dfcb5e
Steve Tom <tomsteve187@gmail.com>
2023-03-07 14:05:44 +0300

    added code for functions and loops

---
+++ nestLoops.cpp
@@ -0,0 +1,14 @@
+// Your First C++ Program
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i=1; i<=5; i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*"<<" ";
+        }
+        cout<<"\t"<<endl;
+    }
+    return 0;
+}
---
+++ squareRoot.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include <cmath>math.h
+using namespace std;
+
+int main() {
+    int num=9;
+    float squareRoot=sqrt(num);
+    cout<<squareRoot;
+    return 0;
+}
